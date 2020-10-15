7024472a40225b6557234f017370ffa262451b18
teerdha1123 <72207277+teerdha1123@users.noreply.github.com>
2020-10-15 14:06:07 +0530

    Add files via upload

---
+++ fang corp.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 50, 50);
+  movingrect=createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingrect.x=mouseX
+  movingrect.y=mouseY
+  if (movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2&&
+    fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2&&movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2)
+    {
+movingrect.shapeColor="red"
+fixedrect.shapeColor="red"
+  }
+  else{
+    movingrect.shapeColor="green"
+    fixedrect.shapeColor="green"
+      
+
+  }
+  
+  drawSprites();
+}
